d2def2688226a7f9f61f8f7613666781ffecdb8d
Gerald Shen <geshen@nvidia.com>
2023-11-20 15:04:34 -0800

    add limit val batches function to supervised trainer and update config
    Signed-off-by: Gerald Shen <geshen@nvidia.com>

--- nemo_aligner/algorithms/supervised.py
+++ nemo_aligner/algorithms/supervised.py
@@ -21,6 +21,7 @@ from tqdm import tqdm
 
 from nemo_aligner.utils.distributed import SyncTimer
 from nemo_aligner.utils.train_utils import clip_gradients
+from nemo_aligner.utils.trainer_utils import compute_limit_batches
 
 
 class SupervisedTrainer:
@@ -57,18 +58,19 @@ class SupervisedTrainer:
 
         # used to compute the max step
         self._train_dataloader_len = len(train_dataloader)
-        self.limit_val_batches = (
-            len(val_dataloader) if self.cfg.limit_val_batches is None else self.cfg.limit_val_batches
-        )
+
+        self.skip_validation = self.cfg.limit_val_batches is not None and self.cfg.limit_val_batches == 0
+        self.limit_val_batches = compute_limit_batches(len(val_dataloader), self.cfg.limit_val_batches)
         self.set_max_steps()
+
+        if not self.skip_validation:
+            assert (
+                self.cfg.save_interval % self.cfg.val_check_interval == 0
+            ), f"{self.cfg.save_interval=} must be divisible by {self.cfg.val_check_interval=}"
 
         self.timer = SyncTimer(
             reduction="mean", sync_cuda=True, buffer_size=1, reduce_op=torch.distributed.ReduceOp.MAX
         )
-
-        assert (
-            self.cfg.save_interval % self.cfg.val_check_interval == 0
-        ), f"{self.cfg.save_interval=} must be divisible by {self.cfg.val_check_interval=}"
 
     def validation_step(self, batch):
         self.model.prepare_for_validation_step()
@@ -172,7 +174,7 @@ class SupervisedTrainer:
 
                 is_train_end = self.step == self.max_steps
                 run_val = (self.step % self.cfg.val_check_interval == 0) or is_train_end
-                if run_val:
+                if run_val and not self.skip_validation:
                     val_loss, val_metrics = self.run_validation()
                     # validation is done on the UPDATED weights
                     # so we use the incremented self.step
@@ -185,7 +187,7 @@ class SupervisedTrainer:
                 # PTL save wants tensors only
                 metrics = {k: torch.as_tensor(v) for k, v in metrics.items()}
 
-                if run_val and (self.step % self.cfg.save_interval == 0 or is_train_end):
+                if (run_val or self.skip_validation) and (self.step % self.cfg.save_interval == 0 or is_train_end):
                     self.save(metrics, is_train_end=is_train_end)
 
                 metrics.clear()
