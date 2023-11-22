5755e85be1c066a02efa1ec40d40d1b521aebd0f
Gerald Shen <geshen@nvidia.com>
2023-11-22 15:14:27 -0800

    reflect change in supervised trainer
    Signed-off-by: Gerald Shen <geshen@nvidia.com>

--- nemo_aligner/algorithms/supervised.py
+++ nemo_aligner/algorithms/supervised.py
@@ -21,7 +21,7 @@ from tqdm import tqdm
 
 from nemo_aligner.utils.distributed import SyncTimer
 from nemo_aligner.utils.train_utils import clip_gradients
-from nemo_aligner.utils.trainer_utils import compute_limit_batches
+from nemo_aligner.utils.trainer_utils import check_progress, compute_limit_batches
 
 
 class SupervisedTrainer:
@@ -60,13 +60,7 @@ class SupervisedTrainer:
         self._train_dataloader_len = len(train_dataloader)
 
         self.limit_val_batches = compute_limit_batches(len(val_dataloader), self.cfg.limit_val_batches)
-        self.skip_validation = self.limit_val_batches == 0
         self.set_max_steps()
-
-        if not self.skip_validation:
-            assert (
-                self.cfg.save_interval % self.cfg.val_check_interval == 0
-            ), f"{self.cfg.save_interval=} must be divisible by {self.cfg.val_check_interval=}"
 
         self.timer = SyncTimer(
             reduction="mean", sync_cuda=True, buffer_size=1, reduce_op=torch.distributed.ReduceOp.MAX
@@ -172,9 +166,15 @@ class SupervisedTrainer:
 
                 self.step += 1
 
-                is_train_end = self.step == self.max_steps
-                run_val = (self.step % self.cfg.val_check_interval == 0) or is_train_end
-                if run_val and not self.skip_validation:
+                run_val, save_model, is_train_end = check_progress(
+                    self.step,
+                    self.max_steps,
+                    self.cfg.val_check_interval,
+                    self.cfg.save_interval,
+                    self.limit_val_batches,
+                )
+
+                if run_val:
                     val_loss, val_metrics = self.run_validation()
                     # validation is done on the UPDATED weights
                     # so we use the incremented self.step
@@ -184,10 +184,9 @@ class SupervisedTrainer:
 
                 global_pbar.set_postfix(metrics)
 
-                # PTL save wants tensors only
-                metrics = {k: torch.as_tensor(v) for k, v in metrics.items()}
-
-                if (run_val or self.skip_validation) and (self.step % self.cfg.save_interval == 0 or is_train_end):
+                if save_model:
+                    # PTL save wants tensors only
+                    metrics = {k: torch.as_tensor(v) for k, v in metrics.items()}
                     self.save(metrics, is_train_end=is_train_end)
 
                 metrics.clear()
