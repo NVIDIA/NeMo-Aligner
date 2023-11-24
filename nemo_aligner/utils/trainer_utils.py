ec6d8dce685a638f4e4fcd8d925867e3951221cc
Gerald Shen <geshen@nvidia.com>
2023-11-23 22:53:36 -0800

    fixup! add check progress function
    Signed-off-by: Gerald Shen <geshen@nvidia.com>

--- nemo_aligner/utils/trainer_utils.py
+++ nemo_aligner/utils/trainer_utils.py
@@ -40,23 +40,23 @@ def safe_is_divisible(a, b):
 def check_progress(
     step: int, max_steps: int, val_check_interval: int, save_interval: int, limit_val_batches: Union[int, float, None]
 ):
-    run_validation = limit_val_batches != 0 and val_check_interval > 0
-    to_save = save_interval > 0
+    is_validation_enabled = limit_val_batches != 0 and val_check_interval > 0
+    is_save_enabled = save_interval > 0
     is_train_end = step == max_steps
 
-    if run_validation:
+    if is_validation_enabled:
         assert save_interval % val_check_interval == 0, f"{save_interval=} must be divisible by {val_check_interval=}"
 
     # run validation on the last step
     # or when we hit the val check interval
     run_val = safe_is_divisible(step, val_check_interval) or is_train_end
-    run_val &= run_validation
+    run_val &= is_validation_enabled
 
     # save model at save intervals or last step
     save_model = safe_is_divisible(step, save_interval) or is_train_end
     # sometimes the user will provide a validation metric
     # to save against, so we need to run val when we save
-    save_model &= run_val or not run_validation
-    save_model &= to_save
+    save_model &= run_val or not is_validation_enabled
+    save_model &= is_save_enabled
 
     return run_val, save_model, is_train_end
