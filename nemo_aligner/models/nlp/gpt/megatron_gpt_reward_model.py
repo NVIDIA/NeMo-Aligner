dfaca13dd1eb90c200188aa84aecce487da9740f
Gerald Shen <geshen@nvidia.com>
2024-01-05 10:39:45 -0800

    fix function name change from nemo
    Signed-off-by: Gerald Shen <geshen@nvidia.com>
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- nemo_aligner/models/nlp/gpt/megatron_gpt_reward_model.py
+++ nemo_aligner/models/nlp/gpt/megatron_gpt_reward_model.py
@@ -318,7 +318,7 @@ class MegatronGPTRewardModel(MegatronGPTModel, SupervisedInterface, Inferrable):
         """
         # mcore uses distributed checkpointing
         if "state_dict" in checkpoint and checkpoint["state_dict"]:
-            for index, module in enumerate(self.get_gpt_module_list()):
+            for index, module in enumerate(self.get_model_module_list()):
                 if parallel_state.get_virtual_pipeline_model_parallel_world_size() is not None:
                     checkpoint_state_dict = checkpoint["state_dict"][f"model_{index}"]
                 else:
