8991eff116e08b03be64b62c66885cf7e4509d36
Yi Dong <yidong@nvidia.com>
2024-01-29 18:55:31 -0800

    show mcts progress
    Signed-off-by: Yi Dong <yidong@nvidia.com>

--- nemo_aligner/utils/deep_search/mcts/mcts.py
+++ nemo_aligner/utils/deep_search/mcts/mcts.py
@@ -18,7 +18,7 @@ from typing import Callable, List
 import numpy as np
 import torch
 import tqdm
-from megatron.core import InferenceParams
+from megatron.core import InferenceParams, parallel_state
 
 
 class ParallelSearch:
@@ -277,8 +277,9 @@ class MCTSParallel:
             spg.root = Node(spg.state, parent=None, action=-1, prior=0.0, visit_count=1)
             spg.root.expand(spg_policy, spg_action)
 
-        for search in range(self.args["num_searches"]):
-
+        dp_rank = parallel_state.get_data_parallel_rank()
+        # use tqdm to show the progresso of the self play
+        for search in tqdm.tqdm(range(self.args["num_searches"]), desc=f"MCTS rank: {dp_rank}", position=dp_rank):
             for spg in ps:
                 # spg.node is to save the node that needs to be expanded
                 spg.node = None
