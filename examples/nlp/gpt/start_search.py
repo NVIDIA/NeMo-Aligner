befe889ddf9ec76585a4a3fb1a1f643ca04cd2d2
Yi Dong <yidong@nvidia.com>
2024-02-26 03:13:30 +0000

    use full
    Signed-off-by: Yi Dong <yidong@nvidia.com>

--- examples/nlp/gpt/start_search.py
+++ examples/nlp/gpt/start_search.py
@@ -13,8 +13,6 @@ def main(cfg):
     dataset = load_dataset("gsm8k", "main")
 
     total = len(dataset)
-    total = 4
-
     data_id_list = torch.tensor(range(total)).split(cfg.model.mcts.rollout_micro_batch_size)  # list of data to process
     search_for_batch = get_search_for_batch(cfg.server_url)
 
@@ -32,24 +30,6 @@ def main(cfg):
                     # results.children.remove(subtask)  # Remove the subtask from the list
             except TimeoutError:
                 pass
-
-
-
-    # job = group(search_for_batch.s(data.tolist()) for data in data_id_list)
-
-    # result = job.apply_async()
-
-    # global_pbar = tqdm(total, desc="Search Global Progress")
-    # while not result.ready():
-    #     for subtask in list(result.children):  # Iterate over a copy of the list
-    #         try:
-    #             if subtask.ready():
-    #                 args = subtask.get()
-    #                 global_pbar.update(cfg.model.mcts.rollout_micro_batch_size)
-    #                 global_pbar.write(f"Finished {args}")
-    #                 result.children.remove(subtask)  # Remove the subtask from the list
-    #         except TimeoutError:
-    #             pass
 
 if __name__ == "__main__":
     main()
