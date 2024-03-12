902b8869d1737e53ef7a4aaaa4e48d07d044e94d
Yi Dong <yidong@nvidia.com>
2024-03-12 14:50:26 +0000

    fix unit-test
    Signed-off-by: Yi Dong <yidong@nvidia.com>

--- tests/test_search_db.py
+++ tests/test_search_db.py
@@ -34,10 +34,10 @@ class TestHistoryTrackingNode:
         assert state[1][1].shape == (11, 10, 10)
         assert state[2][0].shape == (11, 10, 10)
         assert state[2][1].shape == (11, 10, 10)
-        root_node = Node(state=state, parent=None, action=None, prior=0.0, visit_count=0)
+        root_node = Node(state=state, parent=None, action=list(context_id), prior=0.0, visit_count=0)
         search_db.add_root("session1", context_id, root_node)
 
-        full_actions1 = np.arange(0, 1000, 1)
+        full_actions1 = np.arange(1, 1001, 1)
         # shuffle
         np.random.shuffle(full_actions1)
         mock_actions1 = full_actions1[: self.K]
@@ -50,7 +50,7 @@ class TestHistoryTrackingNode:
         depth1_child = child_node
 
         self.child_inference_params.sequence_len_offset += 1
-        full_actions2 = np.arange(0, 1000, 1)
+        full_actions2 = np.arange(1, 1001, 1)
         # shuffle
         np.random.shuffle(full_actions2)
         mock_actions2 = full_actions2[: self.K]
@@ -66,10 +66,9 @@ class TestHistoryTrackingNode:
         K = 30
         selected_actions = np.concatenate([mock_actions1[: K // 2], mock_actions2[: K // 2]], axis=0)
         context_ids = [context_id] * (K // 2) + [child_context_id] * (K // 2)
-        updated_kv_cache, tokens = get_kv_cache(
-            torch.tensor(selected_actions).cuda(), "session1", context_ids, search_db
-        )
+        selected_actions = torch.tensor(selected_actions).cuda().unsqueeze(1)
+        updated_kv_cache, tokens = get_kv_cache(selected_actions, "session1", context_ids, search_db)
         assert len(updated_kv_cache) == 2
-        assert updated_kv_cache[1][0].shape == (13, 30, 10, 10)
-        assert updated_kv_cache[1][1].shape == (13, 30, 10, 10)
+        assert updated_kv_cache[1][0].shape == (14, 30, 10, 10)
+        assert updated_kv_cache[1][1].shape == (14, 30, 10, 10)
         assert tokens.shape == (K, 2)
