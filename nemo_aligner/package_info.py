3b8b70e53dc09db2d4f29208fd00b3b4d20d0f52
Gerald Shen <geshen@nvidia.com>
2024-03-13 16:04:51 -0700

    update package info
    Signed-off-by: Gerald Shen <geshen@nvidia.com>

--- nemo_aligner/package_info.py
+++ nemo_aligner/package_info.py
@@ -14,9 +14,9 @@
 
 
 MAJOR = 0
-MINOR = 1
+MINOR = 2
 PATCH = 0
-PRE_RELEASE = "rc0"
+PRE_RELEASE = ""
 
 # Use the following formatting: (major, minor, patch, pre-release)
 VERSION = (MAJOR, MINOR, PATCH, PRE_RELEASE)
