159df23227b687707bc5b854aa2d3cc13baf392e
Gerald Shen <geshen@nvidia.com>
2024-04-03 16:03:35 -0700

    no drop last
    Signed-off-by: Gerald Shen <geshen@nvidia.com>

--- examples/nlp/gpt/eval_model.py
+++ examples/nlp/gpt/eval_model.py
@@ -183,7 +183,7 @@ def main(cfg) -> None:
         gbs=cfg.model.inference.micro_batch_size * dp_size,
         load_gbs=False,
         collate_fn=collate_fn,
-        drop_last=True,
+        drop_last=False,
         shuffle=False,
     )
 
@@ -196,7 +196,7 @@ def main(cfg) -> None:
         gbs=cfg.model.inference.micro_batch_size * dp_size,
         load_gbs=False,
         collate_fn=collate_fn,
-        drop_last=True,
+        drop_last=False,
         shuffle=False,
     )
 
@@ -209,7 +209,7 @@ def main(cfg) -> None:
         gbs=cfg.model.inference.micro_batch_size * dp_size,
         load_gbs=False,
         collate_fn=collate_fn,
-        drop_last=True,
+        drop_last=False,
         shuffle=False,
     )
 
