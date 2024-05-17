8de7a26187739fcf3b5c9c8ed55b67060efc359f
Gerald Shen <geshen@nvidia.com>
2024-05-16 17:02:12 -0700

    add supress
    Signed-off-by: Gerald Shen <geshen@nvidia.com>

--- examples/nlp/gpt/serve_ppo_critic.py
+++ examples/nlp/gpt/serve_ppo_critic.py
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import torch._dynamo
+
+torch._dynamo.config.suppress_errors = True
+
 from omegaconf.omegaconf import OmegaConf
 
 from nemo.core.config import hydra_runner
