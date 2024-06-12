e09066324f4dab7bf709a24ac7a70cb974eebf67
Gerald Shen <geshen@nvidia.com>
2024-06-12 16:48:50 -0700

    no save topk
    Signed-off-by: Gerald Shen <geshen@nvidia.com>

--- nemo_aligner/utils/utils.py
+++ nemo_aligner/utils/utils.py
@@ -37,23 +37,6 @@ from nemo.utils import AppState, logging
 from nemo.utils.exp_manager import NeMoModelCheckpoint
 from nemo_aligner.models.nlp.gpt.gpt_reward_model import GPTRewardModel
 
-_SAVE_TOP_K = False
-
-
-def is_save_top_k():
-    global _SAVE_TOP_K
-    return _SAVE_TOP_K
-
-
-@contextmanager
-def saving_top_k():
-    global _SAVE_TOP_K
-    try:
-        _SAVE_TOP_K = True
-        yield
-    finally:
-        _SAVE_TOP_K = False
-
 
 class CustomSaveRestoreConnector(NLPSaveRestoreConnector):
     """A save connector that will ask the Reward model to not try to load
@@ -76,13 +59,12 @@ class CustomSaveRestoreConnector(NLPSaveRestoreConnector):
 
 def custom_save_ckpt_func(self, trainer, pl_module, monitor_candidates, is_train_end=False, save_top_only=False):
     """work around used so we can save models manually"""
-    with saving_top_k():
-        super(NeMoModelCheckpoint, self)._save_topk_checkpoint(trainer, monitor_candidates)
+    super(NeMoModelCheckpoint, self)._save_topk_checkpoint(trainer, monitor_candidates)
 
     if save_top_only:
         return
 
-    # super(NeMoModelCheckpoint, self)._save_last_checkpoint(trainer, monitor_candidates)
+    super(NeMoModelCheckpoint, self)._save_last_checkpoint(trainer, monitor_candidates)
 
     if is_train_end:
         # stop the checkpoint logic from saving another last checkpoint
