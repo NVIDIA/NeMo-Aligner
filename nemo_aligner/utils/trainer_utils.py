5f3746b12f825a64758792e70844de66f903054a
Gerald Shen <geshen@nvidia.com>
2024-06-17 21:04:52 -0700

    use compute limit batches
    Signed-off-by: Gerald Shen <geshen@nvidia.com>

--- nemo_aligner/utils/trainer_utils.py
+++ nemo_aligner/utils/trainer_utils.py
@@ -28,17 +28,7 @@ def compute_num_steps_per_epoch(
         raise NotImplementedError("`drop_last=False` is not currently supported")
 
     num_steps_per_epoch = sampler.total_samples // sampler.global_batch_size
-
-    if limit_train_batches is None:
-        limit_train_batches = 1.0
-
-    if limit_train_batches >= 0 and isinstance(limit_train_batches, float):
-        return min(num_steps_per_epoch, int(sampler.total_samples * limit_train_batches) // sampler.global_batch_size)
-    elif limit_train_batches >= 0 and isinstance(limit_train_batches, int):
-        return min(num_steps_per_epoch, limit_train_batches)
-    else:
-        return num_steps_per_epoch
-
+    return compute_limit_batches(num_steps_per_epoch, limit_train_batches)
 
 def compute_limit_batches(number_of_batches: int, limit_batches: Union[int, float, None]):
     if limit_batches is None:
