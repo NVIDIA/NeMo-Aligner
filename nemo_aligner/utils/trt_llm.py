46a91865a24d20914353733b2e40e85f31647641
Terry Kong <terryk@nvidia.com>
2024-07-12 22:41:03 +0000

    instead of hard erroring, send warnings and then override
    Signed-off-by: Terry Kong <terryk@nvidia.com>

--- nemo_aligner/utils/trt_llm.py
+++ nemo_aligner/utils/trt_llm.py
@@ -29,10 +29,12 @@ class GPTGenerateTRTLLM:
         trt_model_dir="/tmp/trt_llm_model",
     ):
         if use_greedy and sample_top_k != 1 and sample_temperature != 0.0:
-            raise ValueError(
-                "If use_greedy=True, you must set sample_top_k=1 and sample_temperature=0.0. "
-                f"Instead found {sample_top_k=} and {sample_temperature=}"
-            )
+            if sample_top_k != 1:
+                logging.warning(f"'use_greedy=True' overrides {sample_top_k=} to 1")
+            if sample_temperature != 0.0:
+                logging.warning(f"'use_greedy=True' overrides {sample_temperature=} to 0.0")
+            sample_top_k = 1
+            sample_temperature = 0.0
 
         self.model_cfg = model_cfg
         self.tokenizer = tokenizer
