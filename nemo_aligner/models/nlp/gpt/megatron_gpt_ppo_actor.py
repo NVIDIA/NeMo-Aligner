984acaa688c335e0bf23354b49182eb0919521b4
Gerald Shen <geshen@nvidia.com>
2024-07-11 17:09:01 -0700

    fix
    Signed-off-by: Gerald Shen <geshen@nvidia.com>

--- nemo_aligner/models/nlp/gpt/megatron_gpt_ppo_actor.py
+++ nemo_aligner/models/nlp/gpt/megatron_gpt_ppo_actor.py
@@ -329,8 +329,8 @@ class MegatronGPTActorModel(NLPAdapterModelMixin, MegatronGPTModel, AlignableGen
             )
             response_tokens = torch.cuda.LongTensor(actor_output["token_ids"]) if actor_output else None
             response_tokens = broadcast_2d_tensor_within_pp(response_tokens, dtype=torch.long)
+            response_lengths = strategy.get_lengths()
 
-        response_lengths = strategy.get_lengths()
         max_response_length = response_lengths.max().item()
 
         # Sanity check to validate response length.
