312efe8b85e1ed3a13e5463f5dc321f5e0b5a0aa
arendu <adithya.r@gmail.com>
2024-08-12 20:53:35 +0000

    mamba dpo
    Signed-off-by: arendu <adithya.r@gmail.com>

--- nemo_aligner/models/nlp/gpt/megatron_gpt_dpo_model.py
+++ nemo_aligner/models/nlp/gpt/megatron_gpt_dpo_model.py
@@ -17,6 +17,7 @@ from functools import partial
 
 import torch
 from megatron.core import parallel_state
+from megatron.core.models.mamba import MambaModel
 from megatron.core.num_microbatches_calculator import get_num_microbatches
 from megatron.core.pipeline_parallel.schedules import get_forward_backward_func
 from omegaconf.dictconfig import DictConfig
@@ -71,6 +72,31 @@ class MegatronGPTDPOModel(NLPAdapterModelMixin, MegatronGPTModel, SupervisedInte
         # variants of preference losses, by default DPO.
         self.preference_loss = self.cfg.dpo.get("preference_loss", "dpo")
         self.gt_reward_scale = self.cfg.dpo.get("gt_reward_scale", 1.0)
+    
+    def model_provider_func(self, pre_process, post_process):
+
+        if self.cfg.mamba_model:
+            from megatron.core.models.mamba.mamba_layer_specs import mamba_stack_spec
+
+            self.hybrid_override_pattern = self.cfg.get(
+                "hybrid_override_pattern", "M" * self.transformer_config.num_layers
+            )
+            self.transformer_config.add_bias_linear = self.cfg.get("add_bias_linear", False)
+            self.transformer_config.gated_linear_unit = self.cfg.get("gated_linear_unit", False)
+            self.transformer_config.layernorm_epsilon = self.cfg.get("layernorm_epsilon", 1e-5)
+
+            model = MambaModel(
+                config=self.transformer_config,
+                max_sequence_length=self.cfg.get("encoder_seq_length", 4096),
+                vocab_size=self.cfg.get("vocab_size", 65536),
+                mamba_ssm_ngroups=self.cfg.get("mamba_ssm_ngroups", 8),
+                mamba_stack_spec=mamba_stack_spec,
+                hybrid_override_pattern=self.hybrid_override_pattern,
+            )
+        else:
+            return super().model_provider_func(pre_process, post_process)
+
+        return model
 
     @torch.no_grad()
     def gather_and_split_rewards(self, pi_logprobs, ref_logprobs, labels, average_log_probs=False):
