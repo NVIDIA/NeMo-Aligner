2bd3059458022a967d43165e8d2b3856b6210f95
Terry Kong <terryk@nvidia.com>
2024-08-21 16:56:39 -0700

    Updates load_checkpoint_model_config to use _filtered_tar_info API
    Signed-off-by: Terry Kong <terryk@nvidia.com>

--- nemo_aligner/utils/utils.py
+++ nemo_aligner/utils/utils.py
@@ -120,7 +120,9 @@ def load_checkpoint_model_config(restore_path):
         return OmegaConf.load(cfg_path)
 
     with tempfile.TemporaryDirectory() as tmpdir:
-        NLPSaveRestoreConnector._unpack_nemo_file(restore_path, tmpdir, extract_config_only=True)
+        # Extracts only model config
+        members = NLPSaveRestoreConnector._filtered_tar_info(restore_path, filter_fn=lambda name: '.yaml' in name)
+        NLPSaveRestoreConnector._unpack_nemo_file(restore_path, tmpdir, members=members)
         cfg = OmegaConf.load(os.path.join(tmpdir, config_name_in_ckpt))
 
     return cfg
