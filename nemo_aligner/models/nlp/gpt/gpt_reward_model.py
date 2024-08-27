a5894c40957a11ac39757e1105484f3448bbe48b
Yi Dong <yidong@nvidia.com>
2024-08-27 10:37:43 -0700

    fix deployment
    Signed-off-by: Yi Dong <yidong@nvidia.com>

--- nemo_aligner/models/nlp/gpt/gpt_reward_model.py
+++ nemo_aligner/models/nlp/gpt/gpt_reward_model.py
@@ -132,7 +132,10 @@ class RewardModelHead(RowParallelLinear):
         if self.output_sequence:
             # a sequence of multi-attribute rewards, used for critic model, returning tensor with shape [B x S]
             assert attributes.dim() == 3, "for critic, attributes should have shape [B x S x self.output_size]"
-            return attributes @ self.attributes_weights
+            if not self.merge_attributes:
+                return attributes
+            else:
+                return attributes @ self.attributes_weights
 
         else:
             assert attributes.dim() == 2, "for reward, attributes should have shape [B x self.output_size]"
