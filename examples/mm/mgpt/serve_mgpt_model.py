07d12c7b70a845bee15cbd7f3f00d964bdfbe95d
Tugrul Konuk <ertkonuk@gmail.com>
2024-09-06 15:54:54 -0500

    MGPT text generation server

---
+++ examples/mm/mgpt/serve_mgpt_model.py
@@ -0,0 +1,240 @@
+# Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import asyncio
+import datetime
+import os
+import threading
+from functools import partial
+
+import torch
+from omegaconf import OmegaConf, open_dict
+from pytorch_lightning.trainer.trainer import Trainer
+
+#from nemo.collections.nlp.models.language_modeling.megatron_gpt_model import MegatronGPTModel
+from nemo.collections.nlp.modules.common.megatron.megatron_init import fake_initialize_model_parallel
+#from nemo.collections.nlp.modules.common.text_generation_server import MegatronServer
+from nemo.collections.nlp.modules.common.text_generation_utils import generate
+from nemo.collections.nlp.modules.common.transformer.text_generation import LengthParam, SamplingParam
+from nemo.collections.nlp.parts.nlp_overrides import CustomProgressBar, NLPDDPStrategy, NLPSaveRestoreConnector
+from nemo.core.config import hydra_runner
+from nemo.utils import AppState, logging
+from nemo.utils.model_utils import inject_model_parallel_rank
+from nemo_aligner.models.mm.mgpt.megatron_mgpt_model import MultimodalGPTModel
+from nemo_aligner.utils.text_generation_utils import MGPTModelTextGenerationStrategy
+from mgpt_generation_server import MegatronMultimodalServer
+
+try:
+    from megatron.core import parallel_state
+
+    HAVE_MEGATRON_CORE = True
+except (ImportError, ModuleNotFoundError):
+    HAVE_MEGATRON_CORE = False
+
+"""Inference server for NeMo-RLHF Initial Policy model.
+"""
+
+if not torch.cuda.is_available():
+    raise OSError("GPU is needed for the inference")
+
+ENDPOINT_BIND_ADDRESS = "0.0.0.0"
+DEFAULT_LOG_FORMAT = "%(asctime)s - %(levelname)8s - %(process)8d - %(threadName)s - %(name)s: %(message)s"
+
+
+def remove_padded_prompts(response, nb_paddings):
+    result = {}
+    for k, v in response.items():
+        if v != None and (type(v) is list or type(v) is torch.Tensor):
+            v = v[:-nb_paddings]
+        result[k] = v
+    return result
+
+
+def init_distributed_parameters(trainer, cfg):
+    app_state = AppState()
+    if (
+        cfg.tensor_model_parallel_size > 1
+        or cfg.pipeline_model_parallel_size > 1
+        or cfg.get("expert_model_parallel_size", 1) > 1
+    ):
+        app_state.model_parallel_size = (
+            cfg.tensor_model_parallel_size
+            * cfg.pipeline_model_parallel_size
+            * cfg.get("expert_model_parallel_size", 1)
+        )
+        app_state.tensor_model_parallel_size = cfg.tensor_model_parallel_size
+        app_state.pipeline_model_parallel_size = cfg.pipeline_model_parallel_size
+        app_state.expert_model_parallel_size = cfg.get("expert_model_parallel_size", 1)
+        (
+            app_state.tensor_model_parallel_rank,
+            app_state.pipeline_model_parallel_rank,
+            app_state.expert_model_parallel_rank,
+            app_state.model_parallel_size,
+            app_state.data_parallel_size,
+            app_state.pipeline_model_parallel_split_rank,
+            app_state.virtual_pipeline_model_parallel_rank,
+        ) = fake_initialize_model_parallel(
+            world_size=app_state.model_parallel_size,
+            rank=trainer.global_rank,
+            tensor_model_parallel_size_=cfg.tensor_model_parallel_size,
+            pipeline_model_parallel_size_=cfg.pipeline_model_parallel_size,
+            pipeline_model_parallel_split_rank_=cfg.pipeline_model_parallel_split_rank,
+        )
+    # if trainer.num_devices and trainer.num_nodes:
+    #     app_state.world_size = trainer.num_devices * trainer.num_nodes
+
+
+@hydra_runner(config_path="conf", config_name="mgpt_inference")
+def main(cfg) -> None:
+    logging.info("\n\n************** Inference configuration ***********")
+    logging.info(f"\n{OmegaConf.to_yaml(cfg)}")
+    # trainer required for restoring model parallel models
+    trainer = Trainer(
+        strategy=NLPDDPStrategy(timeout=datetime.timedelta(seconds=18000)),
+        **cfg.trainer,
+        callbacks=[CustomProgressBar()],
+    )
+
+    if cfg.gpt_model_file is not None:
+        if (
+            cfg.tensor_model_parallel_size < 0
+            or cfg.pipeline_model_parallel_size < 0
+            or cfg.get("pipeline_model_parallel_split_rank", -1) < 0
+        ):
+            save_restore_connector = NLPSaveRestoreConnector()
+            if os.path.isdir(cfg.gpt_model_file):
+                save_restore_connector.model_extracted_dir = cfg.gpt_model_file
+            model_config = MultimodalGPTModel.restore_from(
+                restore_path=cfg.gpt_model_file,
+                trainer=trainer,
+                return_config=True,
+                save_restore_connector=save_restore_connector,
+            )
+
+            # with dist checkpointing we don't need to set this
+            if not model_config.get("mcore_gpt", False):
+                with open_dict(cfg):
+                    cfg.tensor_model_parallel_size = model_config.get("tensor_model_parallel_size", 1)
+                    cfg.pipeline_model_parallel_size = model_config.get("pipeline_model_parallel_size", 1)
+                    cfg.pipeline_model_parallel_split_rank = model_config.get("pipeline_model_parallel_split_rank", 0)
+
+    assert (
+        cfg.trainer.devices * cfg.trainer.num_nodes
+        == cfg.tensor_model_parallel_size * cfg.pipeline_model_parallel_size
+    ), "devices * num_nodes should equal tensor_model_parallel_size * pipeline_model_parallel_size"
+
+    if cfg.gpt_model_file:
+        save_restore_connector = NLPSaveRestoreConnector()
+        if os.path.isdir(cfg.gpt_model_file):
+            save_restore_connector.model_extracted_dir = cfg.gpt_model_file
+
+        pretrained_cfg = MultimodalGPTModel.restore_from(
+            restore_path=cfg.gpt_model_file,
+            trainer=trainer,
+            return_config=True,
+            save_restore_connector=save_restore_connector,
+        )
+        OmegaConf.set_struct(pretrained_cfg, True)
+        with open_dict(pretrained_cfg):
+            pretrained_cfg.sequence_parallel = False
+            pretrained_cfg.activations_checkpoint_granularity = None
+            pretrained_cfg.activations_checkpoint_method = None
+            pretrained_cfg.precision = trainer.precision
+            if pretrained_cfg.get("mcore_gpt", False):
+                # with dist checkpointing we can use the model parallel config specified by the user
+                pretrained_cfg.tensor_model_parallel_size = cfg.tensor_model_parallel_size
+                pretrained_cfg.pipeline_model_parallel_size = cfg.pipeline_model_parallel_size
+            if trainer.precision == "16":
+                pretrained_cfg.megatron_amp_O2 = False
+            elif trainer.precision in ["bf16", "bf16-mixed"] and cfg.get("megatron_amp_O2", False):
+                pretrained_cfg.megatron_amp_O2 = True
+
+            pretrained_cfg.mm_cfg.llm.from_pretrained = cfg.get("base_model_file", None)
+            if cfg.vision_encoder:
+                pretrained_cfg.mm_cfg.vision_encoder.from_pretrained = cfg.get("vision_encoder", None) 
+                
+        model = MultimodalGPTModel.restore_from(
+            restore_path=cfg.gpt_model_file,
+            trainer=trainer,
+            override_config_path=pretrained_cfg,
+            save_restore_connector=save_restore_connector,
+            strict=False,
+            map_location=f"cuda:{trainer.local_rank}",  # map_location is needed for converted models
+        )
+    elif cfg.checkpoint_dir:
+        # checkpoint_path = os.path.join(cfg.checkpoint_dir, cfg.checkpoint_name)
+        # trainer._checkpoint_connector.restore(checkpoint_path)
+        # trainer._checkpoint_connector._restore_modules_and_callbacks(cfg.checkpoint_dir)
+        init_distributed_parameters(trainer, cfg)
+        checkpoint_path = os.path.join(cfg.checkpoint_dir, cfg.checkpoint_name)
+        # checkpoint_path is a dir in case of distributed checkpointing
+        if not os.path.isdir(checkpoint_path):
+            # legacy checkpoint needs model parallel rank injection
+            checkpoint_path = inject_model_parallel_rank(os.path.join(cfg.checkpoint_dir, cfg.checkpoint_name))
+        # overwrite parameters
+        overwrite_cfg = {
+            "sequence_parallel": False,
+            "activations_checkpoint_granularity": None,
+            "activations_checkpoint_method": None,
+            "precision": trainer.precision,
+            "tensor_model_parallel_size": cfg.tensor_model_parallel_size,
+            "pipeline_model_parallel_size": cfg.pipeline_model_parallel_size,
+            "megatron_amp_O2": cfg.get("megatron_amp_O2", False),
+            "tokenizer": cfg.tokenizer,
+        }
+        model = MultimodalGPTModel.load_from_checkpoint(
+            checkpoint_path, hparams_file=None, trainer=trainer, **overwrite_cfg
+        )
+        # model = MultimodalGPTModel.load_from_checkpoint(checkpoint_path, hparams_file=cfg.hparams_file, trainer=trainer)
+        # # the world_size is set to 1, because it read from trainer.world_size when loading the checkponts
+        # init_distributed_parameters(trainer, cfg)
+    else:
+        raise ValueError("need at least a nemo file or checkpoint dir")
+
+    model.freeze()
+
+    # Have to turn off activations_checkpoint_method for inference
+    try:
+        model.model.language_model.encoder.activations_checkpoint_method = None
+    except AttributeError:
+        pass
+
+    fp8_enabled = hasattr(model.cfg, "fp8") and (model.cfg.fp8 == True)
+    if fp8_enabled:
+        nb_paddings = 0
+        while len(cfg.prompts) % 8 != 0:
+            cfg.prompts.append("")
+            nb_paddings += 1
+    
+    # Text generation strategy
+    strategy = MGPTModelTextGenerationStrategy(model)
+    # Third method of running text generation, use inference server
+    if cfg.server:
+        from nemo.collections.nlp.modules.common.megatron_web_server import get_chatbot_demo, get_demo
+
+        if parallel_state.is_pipeline_first_stage() and parallel_state.get_tensor_model_parallel_rank() == 0:
+            if cfg.web_server:
+                raise NotImplementedError(f"Web Server is not supported. Set web_server to False.")
+            server = MegatronMultimodalServer(model.cuda(), inference_strategy=strategy)
+            server.run("0.0.0.0", port=cfg.port)
+
+        while True:
+            choice = torch.cuda.LongTensor(1)
+            torch.distributed.broadcast(choice, 0)
+            if choice[0].item() == 0:
+                generate(model.cuda())
+
+
+if __name__ == "__main__":
+    with torch.no_grad():
+        main()  # noqa pylint: disable=no-value-for-parameter
---
+++ nemo_aligner/servers/mgpt_generation_server.py
@@ -0,0 +1,219 @@
+# Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Utilities for generating text."""
+
+import json
+import threading
+
+import torch
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+import base64
+from io import BytesIO
+from PIL import Image
+
+from nemo.collections.nlp.modules.common.text_generation_utils import generate
+from nemo.collections.nlp.modules.common.text_generation_server import MegatronGenerate, GENERATE_NUM, API_ALLOWED_KEYS, lock
+from nemo.utils import logging
+
+
+class MegatronMultimodalGenerate(MegatronGenerate):
+    def __init__(self, model, inference_strategy=None):
+        super().__init__(model, inference_strategy)
+
+    def put(self):
+        logging.info("request IP: " + str(request.remote_addr))
+        #logging.info(json.dumps(request.get_json()))
+        # check keys
+        for key in request.get_json().keys():
+            if key not in API_ALLOWED_KEYS:
+                logging.error(f"The request key {key} is not allowed")
+
+        sentences = request.get_json()["sentences"]
+        if isinstance(sentences, tuple):  # Input can be text or tensor
+            if len(sentences[0]) != len(sentences[1]) or sentences[0] > 128:
+                return "Maximum number of sentences is 128", 400
+        elif len(sentences) > 128:
+            return "Maximum number of sentences is 128", 400
+
+        task_ids = None  # Used for ptuned/prompt tuned models only
+        if "task_ids" in request.get_json():
+            task_ids = request.get_json()["task_ids"]
+            if not isinstance(sentences, tuple):
+                return "Input at 'sentences' must by a tuple of two tensors like:\
+                    (context_tokens_tensor, context_length_tensor) if task ids are given"
+            if len(task_ids) != len(sentences[0]):
+                return "Each sentence must have a corresponding task id for p-tuned/prompt-tuned models"
+
+        tokens_to_generate = 64  # Choosing hopefully sane default.  Full sequence is slow
+        if "tokens_to_generate" in request.get_json():
+            tokens_to_generate = request.get_json()["tokens_to_generate"]
+            if not isinstance(tokens_to_generate, int):
+                return "tokens_to_generate must be an integer greater than 0"
+            if tokens_to_generate < 1:
+                return "tokens_to_generate must be an integer greater than 0"
+
+        all_probs = False
+        if "all_probs" in request.get_json():
+            all_probs = request.get_json()["all_probs"]
+            if not isinstance(all_probs, bool):
+                return "all_probs must be a boolean value"
+
+        temperature = 1.0
+        if "temperature" in request.get_json():
+            temperature = request.get_json()["temperature"]
+            if not (type(temperature) == int or type(temperature) == float):
+                return "temperature must be a positive number less than or equal to 100.0"
+            if not (0.0 < temperature <= 100.0):
+                return "temperature must be a positive number less than or equal to 100.0"
+
+        add_BOS = False
+        if "add_BOS" in request.get_json():
+            add_BOS = request.get_json()["add_BOS"]
+            if not isinstance(add_BOS, bool):
+                return "add_BOS must be a boolean value"
+
+        greedy = False
+        if "greedy" in request.get_json():
+            greedy = request.get_json()["greedy"]
+            if not isinstance(greedy, bool):
+                return "greedy must be a boolean value"
+
+        top_k = 0
+        if "top_k" in request.get_json():
+            top_k = request.get_json()["top_k"]
+            if not (type(top_k) == int or type(top_k) == float):
+                return "top_k must be a positive integer number"
+            if not (0 <= top_k):
+                return "top_k must be a positive integer number"
+
+        top_p = 0.9
+        if "top_p" in request.get_json():
+            top_p = request.get_json()["top_p"]
+            if not (type(top_p) == int or type(top_p) == float):
+                return "top_p must be a positive number less than or equal to 1.0"
+            if not (0.0 <= top_p <= 1.0):
+                return "top_p must be a positive number less than or equal to 1.0"
+
+        repetition_penalty = 1.2
+        if "repetition_penalty" in request.get_json():
+            repetition_penalty = request.get_json()["repetition_penalty"]
+            if not (type(repetition_penalty) == int or type(repetition_penalty) == float):
+                return "repetition_penalty must be a positive number no less than 1.0"
+            if not (1.0 <= repetition_penalty):
+                return "repetition_penalty must be a positive number no less than 1.0"
+
+        end_strings = ['<|endoftext|>']
+        if 'end_strings' in request.get_json():
+            end_strings = request.get_json()['end_strings']
+            if not isinstance(end_strings, list):
+                return "expect end_strings to be a list of strings"
+            if not all([isinstance(s, str) for s in end_strings]):
+                return "expect end_strings to be a list of strings"
+
+        min_tokens_to_generate = 0
+        if "min_tokens_to_generate" in request.get_json():
+            min_tokens_to_generate = request.get_json()["min_tokens_to_generate"]
+            if not isinstance(min_tokens_to_generate, int):
+                return "min_tokens_to_generate must be an integer no less than 0"
+            if min_tokens_to_generate < 0:
+                return "min_tokens_to_generate must be an integer no less than 0"
+
+        neighbors = None
+        if "neighbors" in request.get_json():
+            neighbors = request.get_json()["neighbors"]
+            if not isinstance(neighbors, int):
+                return "num of neighbors must be an integer no less than 0"
+            if neighbors < 0:
+                return "num of neighbors must be an integer no less than 0"
+
+        compute_logprob = False
+        if "compute_logprob" in request.get_json():
+            compute_logprob = request.get_json()["compute_logprob"]
+            if not isinstance(compute_logprob, bool):
+                return "compute_logprob must be a boolean value"
+
+        random_seed = None
+        if "random_seed" in request.get_json():
+            random_seed = request.get_json()["random_seed"]
+            if random_seed is not None and not isinstance(random_seed, int):
+                return "random_seed must be a positive integer number or None"
+            if random_seed is not None and random_seed < 0:
+                return "random_seed must be a positive integer number or None"
+
+        image_list = None
+        if "images" in request.get_json():
+            base64_images = request.get_json()["images"]
+            image_list = []
+            for base64_string in base64_images:
+                try:
+                    # Decode the base64 string
+                    img_data = base64.b64decode(base64_string)
+                    # Open the image using PIL
+                    img = Image.open(BytesIO(img_data))
+                    # Append to image_list
+                    image_list.append(img)
+                except Exception as e:
+                    logging.error(f"Error processing image: {str(e)}")
+                    return f"Error processing image: {str(e)}", 400
+
+
+        with lock:  # Need to get lock to keep multiple threads from hitting code
+            MegatronGenerate.send_do_generate()  # Tell other ranks we're doing generate
+            extra = {}
+            if task_ids is not None:
+                extra['task_ids'] = task_ids
+            if self.inference_strategy is not None:
+                extra['strategy'] = self.inference_strategy
+            
+            context_tokens_tensor, context_length_tensor = self.inference_strategy.tokenize_batch(
+                sentences, tokens_to_generate, add_BOS
+            )
+
+            output = generate(
+                model=self.model,
+                inputs=(context_tokens_tensor, context_length_tensor),
+                tokens_to_generate=tokens_to_generate,
+                all_probs=all_probs,
+                temperature=temperature,
+                add_BOS=add_BOS,
+                top_k=top_k,
+                top_p=top_p,
+                greedy=greedy,
+                compute_attention_mask=True,
+                compute_logprob=False,
+                repetition_penalty=repetition_penalty,
+                end_strings=end_strings,
+                image_list=image_list,
+                min_tokens_to_generate=min_tokens_to_generate,
+                random_seed=random_seed,
+                **extra,
+            )
+            for k in output:
+                if isinstance(output[k], torch.Tensor):
+                    output[k] = output[k].tolist()
+        if not all_probs:
+            del output['full_logprob']
+
+        return jsonify(output)
+
+
+class MegatronMultimodalServer(object):
+    def __init__(self, model, inference_strategy=None):
+        self.app = Flask(__name__, static_url_path='')
+        api = Api(self.app)
+        api.add_resource(MegatronMultimodalGenerate, '/generate', resource_class_args=[model, inference_strategy])
+
+    def run(self, url, port=5000):
+        self.app.run(url, threaded=True, port=port, debug=False)
