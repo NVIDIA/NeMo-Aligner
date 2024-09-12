1d0be61028dbf16cbf586c3b5c181a2a280c50ae
Tugrul Konuk <ertkonuk@gmail.com>
2024-09-11 23:18:37 -0500

    Remove logging

--- nemo_aligner/models/mm/mgpt/megatron_mgpt_dpo_model.py
+++ nemo_aligner/models/mm/mgpt/megatron_mgpt_dpo_model.py
@@ -21,7 +21,7 @@ from megatron.core.num_microbatches_calculator import get_num_microbatches
 from megatron.core.pipeline_parallel.schedules import get_forward_backward_func
 from omegaconf.dictconfig import DictConfig
 from pytorch_lightning.trainer.trainer import Trainer
-from nemo.utils import logging
+
 from nemo.collections.nlp.modules.common.megatron.utils import (
     average_losses_across_data_parallel_group,
     get_iterator_k_split,
