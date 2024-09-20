ab509e3d00f4b084fa811b2c8073b57f1e89ec82
Gerald Shen <geshen@nvidia.com>
2024-09-20 16:23:32 -0700

    add changes for exps
    Signed-off-by: Gerald Shen <geshen@nvidia.com>

--- nemo_aligner/utils/trt_llm.py
+++ nemo_aligner/utils/trt_llm.py
@@ -83,7 +83,7 @@ class GPTGenerateTRTLLM:
         rng_generator.manual_seed(seed)
         self.rng_generator = rng_generator
 
-        self.pad_id = tokenizer.pad_id
+        self.pad_id = 128255
         end_id = tokenizer.eos_id
         end_strings = list(end_strings)
 
