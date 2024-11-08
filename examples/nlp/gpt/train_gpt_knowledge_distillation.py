51a64f28ebd12726860104fa314a772c9e25476a
Anna Shors <71393111+ashors1@users.noreply.github.com>
2024-11-08 09:26:16 -0800

    feat: add knowledge distillation support for SFT (#348)
    Signed-off-by: Shengyang Sun <shengyangs@nvidia.com>
    Signed-off-by: shengyangs <shengyangs@nvidia.com>
    Signed-off-by: ashors1 <ashors@nvidia.com>
    Signed-off-by: Anna Shors <ashors@nvidia.com>
    Signed-off-by: NeMo-Aligner CI <nemo-aligner-ci@nvidia.com>
    Co-authored-by: Shengyang Sun <shengyangs@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Signed-off-by: abukharin <abukharin@nvidia.com>

---
+++ examples/nlp/gpt/train_gpt_knowledge_distillation.py
@@ -0,0 +1,205 @@
+# Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from functools import partial
+
+import torch
+import torch.multiprocessing as mp
+from omegaconf.omegaconf import OmegaConf
+
+from nemo.collections.nlp.modules.common.megatron.utils import get_ltor_masks_and_position_ids
+from nemo.core.config import hydra_runner
+from nemo.utils import logging
+from nemo.utils.exp_manager import exp_manager
+from nemo_aligner.algorithms.supervised import SupervisedTrainer
+from nemo_aligner.data.nlp.builders import build_dataloader, build_train_valid_test_knowledge_distillation_datasets
+from nemo_aligner.models.nlp.gpt.megatron_gpt_knowledge_distillation import GPTKnowledgeDistillationModel
+from nemo_aligner.utils.distributed import Timer
+from nemo_aligner.utils.train_script_utils import (
+    CustomLoggerWrapper,
+    add_custom_checkpoint_callback,
+    extract_optimizer_scheduler_from_ptl_model,
+    init_distributed,
+    init_peft,
+    init_using_ptl,
+    resolve_and_create_trainer,
+    retrieve_custom_trainer_state_dict,
+)
+from nemo_aligner.utils.utils import load_and_override_model_config, load_from_nemo
+
+"""Script to start knowledge_distillation training"""
+
+OmegaConf.register_new_resolver("multiply", lambda x, y: x * y, replace=True)
+OmegaConf.register_new_resolver("int_div", lambda x, y: x // y, replace=True)
+
+mp.set_start_method("spawn", force=True)
+
+
+def _collate_fn(batch, eos_id, reset_position_ids=False, reset_attention_mask=False, eod_mask_loss=False):
+    tokens = [item["tokens"] for item in batch]
+    labels = [item["labels"] for item in batch]
+    loss_mask = [item["loss_mask"] for item in batch]
+
+    topk_logits = [item["topk_logits"] for item in batch]
+    topk_token_ids = [item["topk_token_ids"] for item in batch]
+    log_sum_exp_logits = [item["log_sum_exp_logits"] for item in batch]
+
+    tokens = torch.nn.utils.rnn.pad_sequence(tokens, batch_first=True, padding_value=eos_id)
+    labels = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True, padding_value=eos_id)
+    loss_mask = torch.nn.utils.rnn.pad_sequence(loss_mask, batch_first=True, padding_value=0)
+    assert len(tokens.shape) == 2, "tokens size should be [B, seq_length], got {tokens.shape}"
+    assert len(labels.shape) == 2, "labels size should be [B, seq_length], got {labels.shape}"
+    assert len(loss_mask.shape) == 2, "loss_mask size should be [B, seq_length], got {loss_mask.shape}"
+
+    topk_logits = torch.nn.utils.rnn.pad_sequence(topk_logits, batch_first=True, padding_value=0)
+    topk_token_ids = torch.nn.utils.rnn.pad_sequence(topk_token_ids, batch_first=True, padding_value=eos_id)
+    assert len(topk_logits.shape) == 3, "topk_logits size should be [B, seq_length], got {topk_logits.shape}"
+    assert len(topk_token_ids.shape) == 3, "topk_token_ids size should be [B, seq_length], got {topk_token_ids.shape}"
+
+    attention_mask, _, position_ids = get_ltor_masks_and_position_ids(
+        tokens, eos_id, reset_position_ids, reset_attention_mask, eod_mask_loss,
+    )
+    assert attention_mask.ndim == 4, "attention_mask is incorrect shape for dpo_custom_collate"
+    if attention_mask.shape[0] == 1:
+        # using .expand() here causes errors from pin_memory=True, so need to use .repeat()
+        # attention_mask = attention_mask.expand(len(batch), *((-1,) * (len(attention_mask.shape) - 1)))
+        attention_mask = attention_mask.repeat(len(batch), *((1,) * (len(attention_mask.shape) - 1)))
+
+    output = {
+        "tokens": tokens,
+        "labels": labels,
+        "loss_mask": loss_mask,
+        "topk_logits": topk_logits,
+        "topk_token_ids": topk_token_ids,
+        "log_sum_exp_logits": log_sum_exp_logits,
+        "attention_mask": attention_mask,
+        "position_ids": position_ids,
+    }
+    return output
+
+
+@hydra_runner(config_path="conf", config_name="gpt_knowledge_distillation")
+def main(cfg) -> None:
+    cfg.model = load_and_override_model_config(cfg.pretrained_checkpoint.restore_from_path, cfg.model)
+
+    logging.info("\n\n************** Experiment configuration ***********")
+    logging.info(f"\n{OmegaConf.to_yaml(cfg)}")
+
+    trainer = resolve_and_create_trainer(cfg, "knowledge_distillation")
+    exp_manager(trainer, cfg.exp_manager)
+    logger = CustomLoggerWrapper(trainer.loggers)
+
+    ptl_model = load_from_nemo(
+        GPTKnowledgeDistillationModel,
+        cfg.model,
+        trainer,
+        strict=True,
+        load_base_model_only=False,
+        restore_path=cfg.pretrained_checkpoint.restore_from_path,
+    )
+
+    init_peft(ptl_model, cfg.model)
+
+    # pull values from checkpoint
+    trainer_restore_path = trainer.ckpt_path
+
+    # TODO: log this restore path
+    if trainer_restore_path is not None:
+        custom_trainer_state_dict = retrieve_custom_trainer_state_dict(trainer)
+        consumed_samples = custom_trainer_state_dict["consumed_samples"]
+    else:
+        custom_trainer_state_dict = None
+        consumed_samples = 0
+
+    init_distributed(trainer, ptl_model, cfg.model.get("transformer_engine", False))
+
+    # use the entire dataset
+    train_valid_test_num_samples = [-1 * cfg.model.global_batch_size] * 3
+
+    train_ds, validation_ds, _ = build_train_valid_test_knowledge_distillation_datasets(
+        cfg=cfg.model,
+        data_prefix=cfg.model.data.data_prefix,
+        data_impl=cfg.model.data.data_impl,
+        splits_string=cfg.model.data.splits_string,
+        train_valid_test_num_samples=train_valid_test_num_samples,
+        seq_length=cfg.model.data.seq_length,
+        seed=cfg.model.seed,
+        tokenizer=ptl_model.tokenizer,
+        n_chunks=cfg.model.data.n_chunks,
+        n_examples_per_chunk=cfg.model.data.n_examples_per_chunk,
+    )
+
+    train_dataloader = build_dataloader(
+        cfg=cfg,
+        dataset=train_ds,
+        consumed_samples=consumed_samples,
+        mbs=cfg.model.micro_batch_size,
+        gbs=cfg.model.global_batch_size,
+        load_gbs=True,
+        pad_samples_to_global_batch_size=False,
+        collate_fn=partial(
+            _collate_fn,
+            eos_id=ptl_model.tokenizer.eos_id,
+            reset_position_ids=cfg.model.data.get("reset_position_ids", False),
+            reset_attention_mask=cfg.model.data.get("reset_attention_mask", False),
+            eod_mask_loss=cfg.model.data.get("eod_mask_loss", False),
+        ),
+    )
+
+    val_dataloader = build_dataloader(
+        cfg=cfg,
+        dataset=validation_ds,
+        consumed_samples=0,
+        mbs=cfg.model.micro_batch_size,
+        gbs=cfg.model.global_batch_size,
+        load_gbs=True,
+        pad_samples_to_global_batch_size=False,
+        collate_fn=partial(
+            _collate_fn,
+            eos_id=ptl_model.tokenizer.eos_id,
+            reset_position_ids=cfg.model.data.get("reset_position_ids", False),
+            reset_attention_mask=cfg.model.data.get("reset_attention_mask", False),
+            eod_mask_loss=cfg.model.data.get("eod_mask_loss", False),
+        ),
+        use_random_sampler=False,
+    )
+
+    init_using_ptl(trainer, ptl_model, train_dataloader, train_ds)
+    optimizer, scheduler = extract_optimizer_scheduler_from_ptl_model(ptl_model)
+
+    ckpt_callback = add_custom_checkpoint_callback(trainer, ptl_model)
+
+    logger.log_hyperparams(OmegaConf.to_container(cfg))
+
+    timer = Timer(cfg.exp_manager.get("max_time_per_run") if cfg.exp_manager else None)
+    kd_trainer = SupervisedTrainer(
+        cfg=cfg.trainer.knowledge_distillation,
+        model=ptl_model,
+        optimizer=optimizer,
+        scheduler=scheduler,
+        train_dataloader=train_dataloader,
+        val_dataloader=val_dataloader,
+        test_dataloader=None,
+        logger=logger,
+        ckpt_callback=ckpt_callback,
+        run_timer=timer,
+    )
+
+    if custom_trainer_state_dict is not None:
+        kd_trainer.load_state_dict(custom_trainer_state_dict)
+
+    kd_trainer.fit()
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/functional/kd.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+set -eoux pipefail
+
+export NCCL_ALGO=Tree
+export NVTE_APPLY_QK_LAYER_SCALING=1
+
+PRETRAINED_CHECKPOINT_NEMO_FILE=${PRETRAINED_CHECKPOINT_NEMO_FILE}
+
+#MIN_LR=$(awk -v var="$LR" 'BEGIN {print var - 1e-11}')
+
+TRAIN_DATA_PATH=$SCRIPT_DIR/test_data/synthetic-123-kd-CHUNK_ID.jsonl
+VALID_DATA_PATH=$SCRIPT_DIR/test_data/synthetic-123-kd-CHUNK_ID.jsonl
+
+NAME="llama3_kd_test"
+
+# PARAMETERS
+RESULTS_DIR="/tmp/${NAME}"
+LOGITS_DATA_DIR="/tmp/${NAME}_data"
+mkdir -p $RESULTS_DIR
+
+GPFS=$(git rev-parse --show-toplevel)
+
+# W&B Logging
+PROJECT=kd_test
+
+# START HETEROGENEUS JOB 3
+CONF_DIR="${GPFS}/examples/nlp/gpt/conf/"
+CONF_NAME="gpt_kd"
+
+CHECKPOINT_DIR="${RESULTS_DIR}/checkpoints"
+TENSOBOARD_DIR="${RESULTS_DIR}/tensorboard"
+
+mkdir -p $RESULTS_DIR
+mkdir -p $TENSOBOARD_DIR
+mkdir -p $CHECKPOINT_DIR
+
+## note: for convenience, the teacher and the student are the same model in this example
+## this should never be done in practice
+
+kd() {
+export CUDA_VISIBLE_DEVICES=0,1
+export PYTHONPATH="${GPFS}:${PYTHONPATH:-}"
+export HYDRA_FULL_ERROR=1
+torchrun --nproc-per-node 2 ${GPFS}/examples/nlp/synthetic_data_gen/compute_topk_logits.py \
+    trainer.num_nodes=1 \
+    trainer.devices=2 \
+    trainer.precision=bf16 \
+    pretrained_checkpoint.restore_from_path=${PRETRAINED_CHECKPOINT_NEMO_FILE} \
+    model.megatron_amp_O2=True \
+    data.sample=True \
+    data.num_workers=0 \
+    data.data.max_seq_length=128 \
+    data.data.file_path=$SCRIPT_DIR/test_data/synthetic-kd.jsonl \
+    data.data.add_eos=False \
+    data.data.hf_dataset=True \
+    top_k=4 \
+    model.global_batch_size=16 \
+    model.micro_batch_size=2 \
+    start_from_idx=0 \
+    end_at_idx=49 \
+    output_path=${LOGITS_DATA_DIR}/train_with_logits_0.jsonl
+
+TRAIN_DATA_PATH=${LOGITS_DATA_DIR}/train_with_logits_CHUNK_ID.jsonl
+VALID_DATA_PATH=${LOGITS_DATA_DIR}/train_with_logits_CHUNK_ID.jsonl
+
+torchrun --nproc-per-node 2 ${GPFS}/examples/nlp/gpt/train_gpt_knowledge_distillation.py \
+    trainer.num_nodes=1 \
+    trainer.devices=2 \
+    trainer.knowledge_distillation.max_steps=3 \
+    pretrained_checkpoint.restore_from_path=${PRETRAINED_CHECKPOINT_NEMO_FILE} \
+    ++model.data.data_impl=chunked_jsonl \
+    ++model.data.n_chunks=1 \
+    ++"model.data.n_examples_per_chunk={train: 50, validation: 50, test: 50}" \
+    ++model.data.seq_length=128 \
+    ++model.global_batch_size=4 \
+    ++model.micro_batch_size=1 \
+    ++model.mcore_gpt=true \
+    ++model.megatron_amp_O2=true \
+    "model.data.data_prefix={train: [${TRAIN_DATA_PATH}], validation: [${VALID_DATA_PATH}], test: [${VALID_DATA_PATH}]}" \
+    exp_manager.create_checkpoint_callback=False \
+    model.data.num_workers=2 \
+    ++model.tensor_model_parallel_size=1 \
+    ++model.pipeline_model_parallel_size=1 \
+    exp_manager.explicit_log_dir=${RESULTS_DIR} \
+    ++model.activations_checkpoint_granularity=full \
+    ++model.activations_checkpoint_method=uniform \
+    ++model.activations_checkpoint_num_layers=1 \
+    ++model.dist_ckpt_load_strictness=log_all \
+    model.knowledge_distillation.target_logits_scale=1.0 \
+    model.knowledge_distillation.logits_scale=1.0 \
+    model.knowledge_distillation.sft_loss_weight=0.1 \
+    model.knowledge_distillation.kd_loss_weight=1 \
+    model.knowledge_distillation.kd_loss=bwd_kl \
+
+}
+
+log_file=$(mktemp /tmp/kd-log-XXXXXX)
+kd | tee $log_file
