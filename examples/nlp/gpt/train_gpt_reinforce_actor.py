360a34139cdd9d478385c8d6dc0b96dc302866cc
abukharin <abukharin@nvidia.com>
2024-11-13 07:46:22 -0800

    Change timer
    Signed-off-by: abukharin <abukharin@nvidia.com>
    Signed-off-by: Terry Kong <terryk@nvidia.com>

--- examples/nlp/gpt/train_gpt_reinforce_actor.py
+++ examples/nlp/gpt/train_gpt_reinforce_actor.py
@@ -159,7 +159,7 @@ def main(cfg) -> None:
     logger.log_hyperparams(OmegaConf.to_container(cfg))
 
     rm = RemoteGPTRMClient(cfg.remote_rm)
-    timer = Timer(cfg.exp_manager.get("max_time_per_run"))
+    timer = Timer(cfg.exp_manager.get("max_time_per_run") if cfg.exp_manager else None)
 
     batch_iterator_cfg = cfg.trainer.reinforce.get("batch_iterator", {})
     batch_iterator_cls = get_batch_iterator_cls(batch_iterator_cfg)
