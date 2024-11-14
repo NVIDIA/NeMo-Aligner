5721741c73d5aebd120c285a4664562f6a6051cd
arendu <adithya.r@gmail.com>
2024-11-14 18:35:27 +0000

    copy dpo style cfg overrides
    Signed-off-by: arendu <adithya.r@gmail.com>

--- examples/nlp/gpt/train_gpt_sft.py
+++ examples/nlp/gpt/train_gpt_sft.py
@@ -39,8 +39,7 @@ from nemo_aligner.utils.train_script_utils import (
     resolve_and_create_trainer,
     retrieve_custom_trainer_state_dict,
 )
-from nemo_aligner.utils.utils import load_from_nemo
-
+from nemo_aligner.utils.utils import load_and_override_model_config, load_from_nemo
 """Script to start SFT training"""
 
 OmegaConf.register_new_resolver("multiply", lambda x, y: x * y, replace=True)
@@ -115,6 +114,7 @@ def _modify_config(gpt_cfg, cfg, add_cfg_to_tree=False):
 
 @hydra_runner(config_path="conf", config_name="gpt_sft")
 def main(cfg) -> None:
+    cfg.model = load_and_override_model_config(cfg.model.restore_from_path, cfg.model)
     logging.info("\n\n************** Experiment configuration ***********")
     logging.info(f"\n{OmegaConf.to_yaml(cfg)}")
 
@@ -126,17 +126,15 @@ def main(cfg) -> None:
     with open_dict(cfg):
         cfg.model.precision = cfg.trainer.precision
 
-    ptl_model, updated_cfg = load_from_nemo(
+    ptl_model = load_from_nemo(
         MambaSFTModel if cfg.model.mamba_hybrid else GPTSFTModel,
         cfg,
         trainer,
         strict=True,
-        modify_config_fn=_modify_config,
         restore_path=cfg.model.restore_from_path,
-        return_updated_cfg=True,
     )
 
-    init_peft(ptl_model, updated_cfg)
+    init_peft(ptl_model, cfg.model)
 
     with open_dict(cfg):
         # overwrite the model config with the config from the checkpoint
