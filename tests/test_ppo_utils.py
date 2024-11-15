db49e0c177bff025b18d42b9113e43b6e7e1cf27
abukharin <abukharin@nvidia.com>
2024-11-15 11:34:37 -0800

    Add test for baseline calculation
    Signed-off-by: abukharin <abukharin@nvidia.com>

--- tests/test_ppo_utils.py
+++ tests/test_ppo_utils.py
@@ -17,7 +17,7 @@ import math
 import torch
 import torch.nn.functional as F
 
-from nemo_aligner.utils.ppo_utils import calculate_advantages_and_returns, calculate_entropy, calculate_ppo_rewards
+from nemo_aligner.utils.ppo_utils import calculate_advantages_and_returns, calculate_entropy, calculate_ppo_rewards, calculate_rloo_baseline
 
 
 class TestCalculateEntropy:
@@ -120,3 +120,27 @@ class TestCalculateAdvantagesAndReturns:
 
         assert torch.allclose(advantages, gt_advantages), "computed advantage is not the same as hand example"
         assert torch.allclose(returns, gt_advantages + values), "computed returns is not the same as hand example"
+
+
+class TestCalculateRLOOBaseline:
+    def test_calculate_rloo_baseline_small_example(self):
+
+        prompts = torch.Tensor([
+            [1, 0],
+            [1, 0],
+            [0, 1],
+            [1, 0],
+            [1, 0],
+            [0, 1],
+            [0, 1],
+            [0, 1],
+        ])
+
+        rewards = torch.Tensor([1, 0, 2, -3, 5, 7, -1, 0])
+        mask = torch.Tensor([1, 1, 1, 1, 1, 1, 1, 0])
+
+        baseline = calculate_rloo_baseline(prompts, rewards, mask)
+
+        gt_baseline = torch.Tensor([2/3, 1., 3., 2., -2/3, 1/2, 9/2, 8/2])
+
+        assert torch.allclose(baseline, gt_baseline), "computed baseline is not the same as hand example"
