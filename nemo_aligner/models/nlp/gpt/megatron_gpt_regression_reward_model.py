60a9dce125f00f0e4855aed99efbe55ba84cd0bf
Maanu Grover <maanug@nvidia.com>
2024-11-18 14:46:07 -0800

    update ptl import
    Signed-off-by: Maanu Grover <maanug@nvidia.com>

--- nemo_aligner/models/nlp/gpt/megatron_gpt_regression_reward_model.py
+++ nemo_aligner/models/nlp/gpt/megatron_gpt_regression_reward_model.py
@@ -17,7 +17,7 @@ import torch
 from megatron.core.num_microbatches_calculator import get_num_microbatches
 from megatron.core.pipeline_parallel.schedules import get_forward_backward_func
 from omegaconf.dictconfig import DictConfig
-from pytorch_lightning.trainer.trainer import Trainer
+from lightning.pytorch.trainer.trainer import Trainer
 
 from nemo.collections.nlp.modules.common.megatron.utils import (
     average_losses_across_data_parallel_group,
