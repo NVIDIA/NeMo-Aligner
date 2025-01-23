a5c9614833e6b05172d9d3ca08c2b325b0b643c0
Daniel Egert <degert@nvidia.com>
2025-01-23 13:42:20 -0800

    Initial commit for new experimental structure
    Signed-off-by: Daniel Egert <degert@nvidia.com>

--- nemo_aligner/experimental/self_rewarding/train_gpt_self_rewarding.py
+++ nemo_aligner/experimental/self_rewarding/train_gpt_self_rewarding.py
@@ -11,9 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import json
-import os
-import subprocess
 from functools import partial
 
 import torch
@@ -22,12 +19,12 @@ from megatron.core import parallel_state
 from megatron.core.utils import divide
 from omegaconf.omegaconf import OmegaConf, open_dict
 
-from nemo.collections.nlp.models.language_modeling.megatron_gpt_model import MegatronGPTModel
 from nemo.core.config import hydra_runner
 from nemo.utils import logging
 from nemo.utils.exp_manager import exp_manager
-from nemo_aligner.algorithms.generation import GenerationTrainer
+from nemo_aligner.experimental.self_rewarding.self_rewarding import SelfRewardingTrainer
 from nemo_aligner.data.nlp.builders import build_dataloader, build_sft_dataset, collate_with_pad_to_max_batch, identity_collate
+from nemo_aligner.models.nlp.gpt.megatron_gpt_spin_model import MegatronGPTSPINModel
 from nemo_aligner.utils.distributed import Timer
 from nemo_aligner.utils.train_script_utils import (
     CustomLoggerWrapper,
@@ -40,7 +37,14 @@ from nemo_aligner.utils.train_script_utils import (
 )
 from nemo_aligner.utils.utils import load_and_override_model_config, load_from_nemo, retrieve_model_state_dict_in_cpu
 
-"""Script to start Aligner Generation"""
+# crashes with nemotron5 container unless we have this
+try:
+    import torch._dynamo
+    torch._dynamo.config.suppress_errors = True
+except:
+    pass
+
+"""Script to start Self-Rewarding training"""
 
 OmegaConf.register_new_resolver("multiply", lambda x, y: x * y, replace=True)
 OmegaConf.register_new_resolver("int_div", lambda x, y: x // y, replace=True)
@@ -49,45 +53,57 @@ OmegaConf.register_new_resolver("subtract", lambda x, y: x - y, replace=True)
 mp.set_start_method("spawn", force=True)
 
 
-@hydra_runner(config_path="conf", config_name="gpt_generation")
+@hydra_runner(config_path="conf", config_name="gpt_self_rewarding")
 def main(cfg) -> None:
     cfg.model = load_and_override_model_config(cfg.pretrained_checkpoint.restore_from_path, cfg.model)
 
     logging.info("\n\n************** Experiment configuration ***********")
     logging.info(f"\n{OmegaConf.to_yaml(cfg)}")
 
-    trainer = resolve_and_create_trainer(cfg, "generation")
+    trainer = resolve_and_create_trainer(cfg, "self_rewarding")
     exp_manager(trainer, cfg.exp_manager)
     logger = CustomLoggerWrapper(trainer.loggers)
 
     ptl_model = load_from_nemo(
-        MegatronGPTModel, cfg.model, trainer, strict=True, restore_path=cfg.pretrained_checkpoint.restore_from_path,
+        MegatronGPTSPINModel,
+        cfg.model,
+        trainer,
+        strict=True,
+        restore_path=cfg.pretrained_checkpoint.restore_from_path,
     )
 
     with open_dict(cfg):
         # overwrite the model config with the config from the checkpoint
         cfg.model.encoder_seq_length = ptl_model.cfg.encoder_seq_length
 
-    # pull values from checkpoint
-    #trainer_restore_path = trainer.ckpt_path
+    if ptl_model.ref_policy_state_dict is None:
+        ref_policy_state_dict = retrieve_model_state_dict_in_cpu(
+            ptl_model, megatron_amp_O2=cfg.model.get("megatron_amp_O2", False)
+        )
+        ptl_model.ref_policy_state_dict = ref_policy_state_dict
 
-    if os.path.exists(gen_file := os.path.join(cfg.exp_manager.explicit_log_dir, "generations", "generations.jsonl")):
-        js_line = json.loads(subprocess.check_output(["tail", "-1", gen_file]).decode("utf_8"))
-        custom_trainer_state_dict = {"step": js_line["step"], "consumed_samples": js_line["consumed_samples"]}
-        consumed_samples = js_line["consumed_samples"]
+    # pull values from checkpoint
+    trainer_restore_path = trainer.ckpt_path
+
+    # TODO: log this restore path
+    if trainer_restore_path is not None:
+        custom_trainer_state_dict = retrieve_custom_trainer_state_dict(trainer)
+        consumed_samples = custom_trainer_state_dict["consumed_samples"]
     else:
         custom_trainer_state_dict = None
         consumed_samples = 0
 
     init_distributed(trainer, ptl_model, cfg.model.get("transformer_engine", False))
+
     train_data_cfg = cfg.model.data.train_ds
+    val_data_cfg = cfg.model.data.validation_ds
 
     if cfg.model.data.get("sample", False):
         # if it is negative, num_samples is None
-        if cfg.trainer.generation.max_steps < 0:
+        if cfg.trainer.self_rewarding.max_steps < 0:
             num_samples = None
         else:
-            num_samples = cfg.trainer.generation.max_steps * cfg.model.global_batch_size
+            num_samples = cfg.trainer.self_rewarding.max_steps * cfg.model.global_batch_size
     else:
         num_samples = None
     train_ds = build_sft_dataset(
@@ -99,6 +115,30 @@ def main(cfg) -> None:
         special_tokens=cfg.model.data.chat_prompt_tokens,
     )
 
+    if cfg.model.data.get("sample", False):
+        num_samples = cfg.trainer.self_rewarding.limit_val_batches * val_data_cfg.global_batch_size
+    else:
+        num_samples = None
+    validation_ds = build_sft_dataset(
+        val_data_cfg,
+        ptl_model.tokenizer,
+        num_samples,
+        answer_only_loss=True,
+        is_chat=cfg.model.data.chat,
+        special_tokens=cfg.model.data.chat_prompt_tokens,
+    )
+
+    # eos_id = ptl_model.tokenizer.eos_id
+
+    # collate fn to pad to the max seq length in the batch
+    # collate_fn = partial(
+    #    self_rewarding_custom_collate,
+    #    eos_id=eos_id,
+    #    reset_position_ids=cfg.model.data.get("reset_position_ids", False),
+    #    reset_attention_mask=cfg.model.data.get("reset_attention_mask", False),
+    #    eod_mask_loss=cfg.model.data.get("eod_mask_loss", False),
+    # )
+
     train_dataloader = build_dataloader(
         cfg=cfg,
         dataset=train_ds,
@@ -109,30 +149,46 @@ def main(cfg) -> None:
         drop_last=train_data_cfg.drop_last,
         pad_samples_to_global_batch_size=False,
         load_gbs=True,
-        use_random_sampler=False,
+        limit_train_batches=cfg.trainer.self_rewarding.limit_train_batches,
+    )
+
+    val_dataloader = build_dataloader(
+        cfg=cfg,
+        dataset=validation_ds,
+        consumed_samples=0,
+        mbs=val_data_cfg.micro_batch_size,
+        gbs=val_data_cfg.global_batch_size,
+        collate_fn=validation_ds.collate_fn,
+        drop_last=val_data_cfg.drop_last,
+        pad_samples_to_global_batch_size=False,
+        load_gbs=True,
     )
 
     init_using_ptl(trainer, ptl_model, train_dataloader, train_ds)
+    optimizer, scheduler = extract_optimizer_scheduler_from_ptl_model(ptl_model)
 
     ckpt_callback = add_custom_checkpoint_callback(trainer, ptl_model)
 
     logger.log_hyperparams(OmegaConf.to_container(cfg))
     timer = Timer(cfg.exp_manager.get("max_time_per_run") if cfg.exp_manager else None)
 
-    gen_trainer = GenerationTrainer(
-        cfg=cfg.trainer.generation,
+    self_rewarding_trainer = SelfRewardingTrainer(
+        cfg=cfg.trainer.self_rewarding,
         model=ptl_model,
+        optimizer=optimizer,
+        scheduler=scheduler,
         train_dataloader=train_dataloader,
+        val_dataloader=val_dataloader,
+        test_dataloader=None,
         logger=logger,
         ckpt_callback=ckpt_callback,
         run_timer=timer,
-        exp_manager=cfg.exp_manager,
     )
 
     if custom_trainer_state_dict is not None:
-        gen_trainer.load_state_dict(custom_trainer_state_dict)
+        self_rewarding_trainer.load_state_dict(custom_trainer_state_dict)
 
-    gen_trainer.generate()
+    self_rewarding_trainer.fit()
 
 
 if __name__ == "__main__":
