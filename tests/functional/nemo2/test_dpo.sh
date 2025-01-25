61bfb7c03eac1bde1e1244d76d9790df4bbe3fdb
Terry Kong <terryk@nvidia.com>
2025-01-25 19:45:53 +0000

    add pre-commit hook for dpo to make sure it doesn't break on commits
    Signed-off-by: Terry Kong <terryk@nvidia.com>

--- tests/functional/nemo2/test_dpo.sh
+++ tests/functional/nemo2/test_dpo.sh
@@ -1,4 +1,3 @@
-
 GPFS=$(git rev-parse --show-toplevel)
 PYTHONPATH=$GPFS:$PYTHONPATH torchrun --nproc-per-node 1 $GPFS/examples/nlp/gpt/train_gpt_dpo.py \
     --restore-from-path=/mnt/checkpoints/dummy_nemo2
