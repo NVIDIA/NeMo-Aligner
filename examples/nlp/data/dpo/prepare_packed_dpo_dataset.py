21e491d7a9545b1ee6470d36a5d3f708e1bd9cf3
ashors1 <ashors@nvidia.com>
2025-01-30 13:56:38 -0800

    add missing import
    Signed-off-by: ashors1 <ashors@nvidia.com>

--- examples/nlp/data/dpo/prepare_packed_dpo_dataset.py
+++ examples/nlp/data/dpo/prepare_packed_dpo_dataset.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import collections
 import json
 import os
 from dataclasses import dataclass
