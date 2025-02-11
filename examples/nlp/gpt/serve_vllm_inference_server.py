Changed sleep level to 1 for vllm. Seems to fix logprob issues

examples/nlp/gpt/serve_vllm_inference_server.py
@@ -33,6 +33,7 @@ def worker_process(in_queue, out_queue, load_path, tp):
             torch.cuda.empty_cache()
             # Force garbage collection
             gc.collect()
+            torch.cuda.empty_cache()
 
         def reset(self):
             self.model_runner.builder.reset_cached_inter_data()
@@ -68,21 +69,23 @@ def worker_process(in_queue, out_queue, load_path, tp):
                 tensor_parallel_size=tp, 
                 generation_config='auto', 
                 enforce_eager=True, 
-                gpu_memory_utilization=.4, 
+                gpu_memory_utilization=.5, 
                 enable_sleep_mode=True,
             )
             self.running = True
             print("vLLM Inference Server started.")
 
         def sleep(self):
-            self.llm.llm_engine.reset_prefix_cache()
-            for worker in self.llm.llm_engine.model_executor.workers:
-                worker.execute_method("reset")
-            self.llm.llm_engine.model_executor.driver_worker.worker.reset()
-            self.llm.sleep(level=2)
+            #return
+            #self.llm.llm_engine.reset_prefix_cache()
+            #for worker in self.llm.llm_engine.model_executor.workers:
+            #    worker.execute_method("reset")
+            #self.llm.llm_engine.model_executor.driver_worker.worker.reset()
+            self.llm.sleep(level=1)
             self.asleep = True
 
         def wake_up(self):
+            #return
             self.llm.wake_up()
             self.asleep = False
 

nemo_aligner/experimental/grpo/models/nlp/gpt/megatron_gpt_grpo_actor.py
@@ -398,6 +398,7 @@ class MegatronGPTActorModel(NLPAdapterModelMixin, MegatronGPTModel, AlignableGen
                     print(f"CONVERTED {name} with out shape {[(n, v.shape) for n, v in hf_name_param_mapping.items()]}")
                     cpu_param_dict.update({k: v.cpu() for k, v in hf_name_param_mapping.items()})
                     print("Memory free before del", torch.cuda.mem_get_info()[0] / 1024**3, flush=True)
+                    torch.cuda.synchronize()
                     for k, v in hf_name_param_mapping.items():
                         del v
                     print("Memory free after del", torch.cuda.mem_get_info()[0] / 1024**3, flush=True)
@@ -423,7 +424,8 @@ class MegatronGPTActorModel(NLPAdapterModelMixin, MegatronGPTModel, AlignableGen
                 try:
                     # path = os.path.join(out_dir, "params.safetensors")
                     path = out_dir
-                    parallel_save_cpu_state_dict(cpu_param_dict, path, num_chunks=8)
+                    #parallel_save_cpu_state_dict(cpu_param_dict, path, num_chunks=8, delete_existing=True)
+                    save_file(cpu_param_dict, os.path.join(out_dir, "params.safetensors"))
                 except Exception as e:
                     print(f"Error saving params.safetensors: {e}", flush=True)
                     if os.path.exists(path):