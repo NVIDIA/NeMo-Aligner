52eeb664b70d8ad9bfddd57c825d26f0b7324afa
Gerald Shen <geshen@nvidia.com>
2025-02-20 23:06:41 -0800

    fix
    Signed-off-by: Gerald Shen <geshen@nvidia.com>

--- examples/nlp/gpt/train_gpt_grpo.py
+++ examples/nlp/gpt/train_gpt_grpo.py
@@ -166,11 +166,10 @@ def main(cfg) -> None:
     # init environments and rollout generator
     math_environment = MathEnvironment(cfg.trainer.grpo.environments.math)
     code_environment = CodeEnvironment(cfg.trainer.grpo.environments.code)
+
     # your_environment = Environment(cfg)
-    tasks_to_environments = {
-        "math": math_environment,
-        "code": code_environment,
-    }
+    tasks_to_environments = {k:MathEnvironment(cfg.trainer.grpo.environments.math) for k in {"aime24", "amc23", "math", "qwq_sol_gen_no_ans_c4", "qwq_sol_gen_no_ans_c7", "qwq_sol_gen_no_ans_olymp_pr_gt03", "qwq_sol_gen_no_ans_olymp_pr_lt03"}}
+    tasks_to_environments["code"] = CodeEnvironment(cfg.trainer.grpo.environments.code)
     rollout_generator = SequenceRewardRolloutGenerator(cfg.trainer.grpo, tasks_to_environments)
 
     timer = Timer(cfg.exp_manager.get("max_time_per_run") if cfg.exp_manager else None)
